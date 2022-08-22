4608dadef23d432ebbca104c9856559f5b7b436d
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-08-22 23:00:03 +0100

    Completed

--- controllers/userPostReviewController.js
+++ controllers/userPostReviewController.js
@@ -3,8 +3,34 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const Review = require("../models/review.model");
 
-module.exports.postReviews=async(req,res)=>{
+module.exports.postReviews = async (req, res) => {
+  try {
+    if (req.user) {
+      let { _id, isLoggedIn } = req.user;
+      if (isLoggedIn === true) {
+        const user = await User.findOne({ _id });
+        if (user) {
+          let { title, houseLocation, description } = await req.body;
+          const userId = _id;
+          const newReview = new Review({
+            userId,
+            title,
+            houseLocation,
+            description,
+          });
 
-
-
-}
+          newReview.save((err, data) => {
+            errorDey(err);
+            res.json({
+              successful: true,
+              message: `data has been saved`,
+              data,
+            });
+          });
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
